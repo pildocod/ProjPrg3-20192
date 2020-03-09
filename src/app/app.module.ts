bc3c93347e502a270c00524193e6f4c519f3cc02
pildocod <pildorax@gmail.com>
2020-03-09 15:16:54 -0500

    Nope

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { FooterComponent } from './template/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
